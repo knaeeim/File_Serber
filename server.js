45be3319ee2f484fe83867896f9dd28dc260909b
Naeeim <kmnaeeim@gmail.com>
2023-04-30 09:01:56 +0530

    Updated

--- server.js
+++ server.js
@@ -42,33 +42,6 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end("<h1> Not Found </h1>"); 
     }
-    // let filePath;
-  
-    // switch (req.url) {
-    //   case '/':
-    //     filePath = path.join(publicdir, 'index.html');
-    //     break;
-    //   case '/about':
-    //     filePath = path.join(publicdir, 'about.html');
-    //     break;
-    //   case '/contact':
-    //     filePath = path.join(publicdir, 'contact.html');
-    //     break;
-    //   default:
-    //     filePath = path.join(publicdir, req.url);
-    //     break;
-    // }
-  
-    // fs.readFile(filePath, "utf-8", (err, data) => {
-    //   if (err) {
-    //     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    //     res.end('404 Not Found');
-    //     return;
-    //   }
-  
-    //   res.writeHead(200);
-    //   res.end(data);
-    // });
   });
   
     
